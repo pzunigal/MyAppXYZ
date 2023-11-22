2f9e8b8c313b1d322157cb06c1fc690422c354d7
pzunigal <zuniga.lagosp@gmail.com>
2023-11-22 18:23:48 -0300

    Initial commit

---
+++ app/src/main/java/com/example/myappxyz/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.myappxyz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+    FloatingActionButton fab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
