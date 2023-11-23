2684c7235de1126b6177cf159bd2a9c9e3f44e0d
pzunigal <zuniga.lagosp@gmail.com>
2023-11-23 19:09:40 -0300

    x

--- app/src/main/java/com/example/myappxyz/MyAdapter.java
+++ app/src/main/java/com/example/myappxyz/MyAdapter.java
@@ -14,6 +14,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MyAdapter extends RecyclerView.Adapter<MyViewHolder> {
@@ -56,7 +57,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyViewHolder> {
     @Override
     public int getItemCount() { return dataList.size(); }
 
-    public void searchDataList(ArrayList<Dataclass> searchList) {
+    public void searchDataList(ArrayList<DataClass> searchList) {
         dataList = searchList;
         notifyDataSetChanged();
     }
